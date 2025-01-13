8056c2140d8106a03526c5456365668d0cde3bee
fbriansyah <fbriansyah@gmail.com>
2025-01-13 07:21:35 +0700

    formating

--- src/extension.ts
+++ src/extension.ts
@@ -1,31 +1,42 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
-import * as vscode from 'vscode';
-import createIssue from './cmd/createIssue';
-import setupConfiguration from './cmd/setupConfiguration';
-import { IssueViewProvider } from './views/IssueView';
+import * as vscode from "vscode";
+import createIssue from "./cmd/createIssue";
+import setupConfiguration from "./cmd/setupConfiguration";
+import { IssueViewProvider } from "./views/IssueView";
 
 // This method is called when your extension is activated
 // Your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
+  // Use the console to output diagnostic information (console.log) and errors (console.error)
+  // This line of code will only be executed once when your extension is activated
+  console.log(
+    'Congratulations, your extension "issue-reporter-ts" is now active!'
+  );
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "issue-reporter-ts" is now active!');
+  // The command has been defined in the package.json file
+  // Now provide the implementation of the command with registerCommand
+  // The commandId parameter must match the command field in package.json
+  const createIssueCmd = vscode.commands.registerCommand(
+    "issue-reporter-ts.createIssue",
+    createIssue
+  );
+  const setupCmd = vscode.commands.registerCommand(
+    "issue-reporter-ts.setup",
+    setupConfiguration
+  );
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	const createIssueCmd = vscode.commands.registerCommand('issue-reporter-ts.createIssue', createIssue);
-	const setupCmd = vscode.commands.registerCommand('issue-reporter-ts.setup', setupConfiguration);
+  context.subscriptions.push(createIssueCmd);
+  context.subscriptions.push(setupCmd);
 
-	context.subscriptions.push(createIssueCmd);
-	context.subscriptions.push(setupCmd);
-
-		// create issue reporter view
-		const issueReporterViewProvider = new IssueViewProvider(context.extensionUri);
-		context.subscriptions.push(
-			vscode.window.registerWebviewViewProvider(IssueViewProvider.viewType, issueReporterViewProvider));
+  // create issue reporter view
+  const issueReporterViewProvider = new IssueViewProvider(context.extensionUri);
+  context.subscriptions.push(
+    vscode.window.registerWebviewViewProvider(
+      IssueViewProvider.viewType,
+      issueReporterViewProvider
+    )
+  );
 }
 
 // This method is called when your extension is deactivated
